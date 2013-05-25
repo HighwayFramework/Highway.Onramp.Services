10a1296ff7749e8e36623f27f41f5336241a3d66
Tim Rayburn <tim@timrayburn.net>
2013-05-25 16:12:30 -0500

    Fixing missed files

--- src/Templates.Service/Entities/ExampleEntity.cs
+++ src/Templates.Service/Entities/ExampleEntity.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// [[Highway.Onramp.Services.Data]]
+// This is an example entity, nothing more.
+// It is not required for the solution to work, 
+// simply replace it with your own entities.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
